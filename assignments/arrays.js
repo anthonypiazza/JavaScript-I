cceb1946702a096a05f81acac10fab18e061ab8d
anthonypiazza <piazza.anthony.n@gmail.com>
2019-05-06 14:48:42 -0500

    Finished Function Conversion

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -56,14 +56,14 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // Example for loop:
 
-// arr = [1,2,3,4];
-// for (let i = 0; i < arr.length; i++) {
-//     arr[i]; // 1,2,3,4
-// }
+arr = [32];
+for (let i = 0; i < arr.length; i++) {
+    arr[i]; 32
+}
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
+console.log(`Car ${index[32]} is a ${this["car_year"]} ${this["car_make"]} ${this["car_model"]}` );
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
